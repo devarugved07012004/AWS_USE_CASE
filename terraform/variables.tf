a85a7a520f49e56890047da49d073a5bdb1d8dac
Deva Rugved <120798781+devarugved07012004@users.noreply.github.com>
2025-06-20 14:22:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,4 +1,4 @@
 variable "aws_region" {
   description = "The AWS region to deploy resources"
-  default     = "us-west-2"  # 🔁 Replace with your desired AWS region
+  default     = "eu-north-1"  # 🔁 Replace with your desired AWS region
 }
